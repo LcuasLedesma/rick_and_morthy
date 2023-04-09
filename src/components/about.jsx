ff7c97f4f32b6f2ddf97c88e68f6eb8c96ae7b6c
Lucas Ledesma <lucasfidel99@gmail.com>
2023-04-09 08:53:51 -0300

    Rick-and-Morthy app

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import '../css/about.css';
 
 export default function About() {
   return (
-    <div>
-      <h1>Sobre nosotros</h1>
-      <p>Aquí podrías escribir información sobre tu empresa o proyecto.</p>
+    <div className='contenedor'>
+
+      <div className='columna-superior'>
+        <img src="../img/ramas.jpg"/>
+      </div>
+
+      <div className='columna-inferior'>
+        <div className='title'>
+          <h1 >Hi, i'm Lucas!</h1>
+        </div>
+        <div className='parraf1'>
+          <h2>
+            I'm a web developer who is always hungry for knowledge and eager to improve. my commitment to continuous learning and passion is what sets me apart in the industry.
+
+            seeking out new skills and knowledge to stay ahead of the curve.
+
+          </h2>
+        </div>
+
+        <div className='parraf2'>
+          <h2>
+            I'm proficient in a variety of programming languages and frameworks, such as:
+            HTML, CSS, JavaScript, React. Which allows me to tackle any challenge that comes my way.
+            If you're looking for a web developer who is dedicated, passionate, and always pushing the boundaries of what's possible, look no further
+          </h2>
+        </div>
+      </div>
+
     </div>
   );
 }
